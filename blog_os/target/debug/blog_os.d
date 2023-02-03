de857daf97ba1e69cc00782cf51ddab5b52c8264
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-02-03 13:48:00 -0800

    Delete folder

--- blog_os/target/debug/blog_os.d
+++
@@ -1,1 +0,0 @@
-/Users/ariakillebrewbruehl/Documents/spring2023/csci393/blog_os/target/debug/blog_os: /Users/ariakillebrewbruehl/Documents/spring2023/csci393/blog_os/src/main.rs
--- blog_os/target/debug/deps/blog_os-0c29442733261732.d
+++
@@ -1,5 +0,0 @@
-/Users/ariakillebrewbruehl/Documents/spring2023/csci393/blog_os/target/debug/deps/blog_os-0c29442733261732: src/main.rs
-
-/Users/ariakillebrewbruehl/Documents/spring2023/csci393/blog_os/target/debug/deps/blog_os-0c29442733261732.d: src/main.rs
-
-src/main.rs:
